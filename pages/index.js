582a056b748b4677c73f65726d829bad46d37b20
aleksa-codes <stojsic.aleksa@gmail.com>
2023-01-10 19:22:06 +0100

    Fix: small fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Blog from '../components/Blog'
-
 import Header from '../components/Header'
 
 const URL = process.env.STRAPI_BASE_URL
